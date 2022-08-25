e2214669b71eb2c9c0accf61b90d6335d4f10739
thenns <samm77@gmail.com>
2022-08-25 01:05:31 -0600

    started prob 15

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -251,7 +251,9 @@ let bigOrSmallArray = [1,101, 102, 2, 103, 4, 5, 6, 107]
 */
 
 //CODE HERE
-
+function bigOrSmall (arr) {
+    const answers;
+};
 
 ////////////////// PROBLEM 16 ////////////////////
 let contestants = ['Katniss', 'Peeta', 'Fox-face', 'Glimmer', 'Cato', 'Rue', 'Thresh', 'Clove', 'Marvel']
